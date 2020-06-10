31f82a2580dfdab28654068449d73e981e155ef8
juniorjacob <38077688+juniorjacob@users.noreply.github.com>
2020-06-10 10:40:05 -0500

    updated
    new notes, proper readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ret_addr_spoofer
- Compileable POC of namazso's x64 return address spoofer.
+Compileable POC of namazso's x64 return address spoofer. Modified to use the RDX register and includes some helpful notes inside of the MASM shell to explain what lines would need to be modified to work with other registers.
+
+https://www.unknowncheats.me/forum/anti-cheat-bypass/268039-x64-return-address-spoofing-source-explanation.html
--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ void abc() {
 	std::cout << "return address: " << _ReturnAddress() << std::endl; // should print address of jmp_rdx
 }
 
+// limitations: only works on x64, works on only one nonvolatile register (forced to find a part of the module that uses same shell as jmp_rdx), cannot work on any address, easily detected via signature
+
 int main() {
 	std::cout << "main: " << std::hex << (void*)main << std::endl;
 	std::cout << "jmp_rdx: " << (void*)jmp_rdx << std::endl;
